0e4e46aaa096bf2fbef097ae6b9b1223f3f16586
Ahmed Osman <ahmed.osman@kcl.ac.uk>
2022-10-04 09:22:56 +0100

    Add solution to frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,16 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        
+        #Check if frequencies contains item in string form
+        if(frequencies.keys().__contains__(str(item))):
+            #Update count of value by 1.
+            value = frequencies[str(item)]
+            frequencies[str(item)] = value + 1
+        else:
+            #If value key and value is not present, add new key value to dictionary
+            frequencies[str(item)] = 0
+
     return frequencies
+
